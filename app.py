68e022b7148240cf149cf9da9aa97d5dbe3f03fb
Petar Lalov <petar.lalov@gmail.com>
2025-04-19 08:12:57 +0300

    Comments added

--- app.py
+++ app.py
@@ -14,39 +14,27 @@ if "should_clear_selection" not in st.session_state:
     st.session_state.should_clear_selection = False
 
 
-# Function to clear all inputs
-def on_button_send():
-    # If there is user_text, add it to the DataFrame
-    if st.session_state.textinput.strip():
-        # Add with placeholder name
-        st.session_state.df = pd.concat(
-            [
-                st.session_state.df,
-                pd.DataFrame({"name": ["User Input"], "command": [st.session_state.textinput]}),
-            ],
-            ignore_index=True,
-        )
-    st.session_state.should_clear_selection = not st.session_state.should_clear_selection
-    st.session_state.textinput = ""
-
-
-df = st.session_state.df
-
+# Create the view
 st.title("Simple Table with Row Selection")
 
-st.text_area("Enter some text:", key="textinput")
+# Text area for user input
+st.text_area("Enter command:", key="textinput")
 
 
 # Generate a dynamic key based on the clear selection flag
+# This is used to force the dataframe to be re-rendered when the selection changes
+# This is needed to clear the selection when a new row is added
 dataframe_key = f"dataframe_{st.session_state.should_clear_selection}"
 
+# once the rows are selected, we are ready do something with them
+# in this example we are just printing the selected rows
 def on_select():
     event = st.session_state[dataframe_key]
     # Check if rows are selected and display their indices
     if event.selection and event.selection.rows:
         selected_indices = event.selection.rows
         # If you want to access the data of selected rows
-        selected_data = df.iloc[selected_indices]
+        selected_data = st.session_state.df.iloc[selected_indices]
         print("Selected data:", selected_data)
 
 
@@ -58,6 +46,29 @@ st.dataframe(
     on_select=on_select,
     key=dataframe_key,
 )
+
+
+# Function to clear input widget and if there is user_text do new row to the DataFrame
+# This is needed to clear the selection when a new row is added
+def on_button_send():
+    # If there is user_text, add it to the DataFrame
+    if st.session_state.textinput.strip():
+        # Add with placeholder name
+        st.session_state.df = pd.concat(
+            [
+                st.session_state.df,
+                pd.DataFrame(
+                    {"name": ["User Input"], "command": [st.session_state.textinput]}
+                ),
+            ],
+            ignore_index=True,
+        )
+    # Toggle the clear selection flag
+    # This is needed to clear the selection when a new row is added or we send the data
+    st.session_state.should_clear_selection = (
+        not st.session_state.should_clear_selection
+    )
+    st.session_state.textinput = ""
 
 
 def delete_rows():
