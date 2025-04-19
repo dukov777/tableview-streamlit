86a40b05dc99d85573381875f5f939a63c52a69b
Petar Lalov <petar.lalov@gmail.com>
2025-04-19 07:38:34 +0300

    Refactor table selection handling and add pandas series examples

--- app.py
+++ app.py
@@ -40,17 +40,31 @@ st.text_area("Enter some text:", key="textinput")
 # Generate a dynamic key based on the clear selection flag
 dataframe_key = f"dataframe_{st.session_state.should_clear_selection}"
 
-event = st.dataframe(
+def on_select():
+    event = st.session_state[dataframe_key]
+    # Check if rows are selected and display their indices
+    if event.selection and event.selection.rows:
+        selected_indices = event.selection.rows
+        # st.write("Selected row indices:", selected_indices)
+
+        # If you want to access the data of selected rows
+        selected_data = df.iloc[selected_indices]
+        # st.write("Selected data:")
+        # st.write(selected_data)
+
+
+st.dataframe(
     st.session_state.df,
     use_container_width=True,
     height=200,
     selection_mode="multi-row",
-    on_select="rerun",
+    on_select=on_select,
     key=dataframe_key,
 )
 
 
 def delete_rows():
+    event = st.session_state[dataframe_key]
     if event.selection and event.selection.rows:
         selected_indices = event.selection.rows
         st.session_state.df = st.session_state.df.drop(selected_indices).reset_index(
@@ -63,13 +77,3 @@ st.button("Send", on_click=on_button_send)
 
 # button to delete selected row in the table
 st.button("Delete Row", on_click=delete_rows)
-
-# Check if rows are selected and display their indices
-if event.selection and event.selection.rows:
-    selected_indices = event.selection.rows
-    st.write("Selected row indices:", selected_indices)
-
-    # If you want to access the data of selected rows
-    selected_data = df.iloc[selected_indices]
-    st.write("Selected data:")
-    st.write(selected_data)
